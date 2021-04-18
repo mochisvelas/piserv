847ecf80f6309c3f8827dc01bc9bfa531e879c02
mochisvelas <velasquezmochis@gmail.com>
2021-04-18 01:53:45 -0500

    subclient

--- client.py
+++ client.py
@@ -1,7 +1,20 @@
 import requests
+import RPi.GPIO as GPIO
 
-payload = {'key':5}
 
-r = requests.post('http://127.0.0.1:5000/', json=payload)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.OUT)
 
-print(r.json())
+while True:
+    if GPIO.input(20):
+        payload = {'20':'1'}
+        r = requests.post('http://127.0.0.1:5000/', json=payload)
+
+        if r.json()['21'] == '1':
+            GPIO.output(21, True)
+        else:
+            GPIO.output(21, False)
+
+GPIO.cleanup()
