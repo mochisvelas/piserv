5ee72f1a3a6930ad67236a60bfbd630ad1623111
Ubuntu <ubuntu@ip-172-31-20-15.us-east-2.compute.internal>
2021-04-18 23:31:12 +0000

    create maria html

--- sever.py
+++ sever.py
@@ -1,6 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import requests
 import mysql.connector
+import time
+from datetime import datetime
+from json2html import * 
+
+#create table test(id int not null auto_increment, datetime varchar(50), status varchar(50), primary key (id));
+#insert into test(datetime, status) values('gg', 'what');
+db = mysql.connector.connect(
+        host = "localhost",
+        user = "mochis",
+        passwd = "mochis",
+        database = "aws")
+
+mycursor = db.cursor()
 
 app = Flask(__name__)
 
@@ -9,11 +22,16 @@ app = Flask(__name__)
 def index():
     if(request.method == 'POST'):
         pi_json = request.get_json()
+        now = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
         if pi_json['20'] == '1':
             # insert reg in database
+            mycursor.execute("insert into test(datetime, status) values('{}', 'on');".format(str(now)))
+            db.commit()
             return jsonify({'21':'True'}), 201
         else:
             # insert reg in database
+            mycursor.execute("insert into test(datetime, status) values('{}', 'on');".format(str(now)))
+            db.commit()
             return jsonify({'21':'False'}), 201
     else:
         return jsonify({'about':'Hello pimochis'})
@@ -41,9 +59,20 @@ def index():
 #         return jsonify({str(pin): str(energy)}), 201
 
 # query database
-@app.route('/input/<int:pin>/<string:energy>', methods=['GET'])
-def get_input_pin(pin, energy):
-    return jsonify({str(pin): str(energy)})
+#@app.route('/input/<int:pin>/<string:energy>', methods=['GET'])
+#def get_input_pin(pin, energy):
+#    return jsonify({str(pin): str(energy)})
+
+# query database
+@app.route('/maria', methods=['GET'])
+def get_input_pin():
+    data_all = []
+    mycursor.execute("select * from test")
+    for i in mycursor:
+        data_all.append({'id':i[0], 'datetime':i[1], 'status':i[2]})
+    columnNames = ['id', 'datetime', 'status']
+    return render_template('maria.html', records=data_all, colnames=columnNames)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
